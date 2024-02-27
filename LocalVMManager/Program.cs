0323ce46092010d45081583bbc12279ca172ad2d
johnburitto <johnburitto@gmail.com>
2024-02-28 01:25:37 +0200

    Example of installing, use and remove package to local vm

---
+++ LocalVMManager/Program.cs
@@ -0,0 +1,34 @@
+﻿using Renci.SshNet;
+using System.Text.RegularExpressions;
+
+var method = new PasswordAuthenticationMethod("johnburitto", "10utezez");
+var connection = new ConnectionInfo("127.0.0.1", 2000, "johnburitto", method);
+var client = new SshClient(connection);
+
+client.Connect();
+
+IDictionary<Renci.SshNet.Common.TerminalModes, uint> modes =
+new Dictionary<Renci.SshNet.Common.TerminalModes, uint>
+{
+    { Renci.SshNet.Common.TerminalModes.ECHO, 53 }
+};
+
+ShellStream shellStream = client.CreateShellStream("xterm", 80, 24, 800, 600, 1024, modes);
+
+var output = shellStream.Expect(new Regex(@"[$>]"));
+Console.WriteLine(output);
+shellStream.WriteLine("sudo apt install neofetch");
+output = shellStream.Expect(new Regex(@"([$#>:])"));
+Console.WriteLine(output);
+shellStream.WriteLine("10utezez");
+output = shellStream.Expect(new Regex(@"[$>]"));
+Console.WriteLine(output);
+shellStream.WriteLine("apt list | neofetch");
+output = shellStream.Expect(new Regex(@"[$>]"));
+Console.WriteLine(output);
+shellStream.WriteLine("sudo apt remove neofetch");
+output = shellStream.Expect(new Regex(@"([$#>:])"));
+Console.WriteLine(output);
+shellStream.WriteLine("Y");
+output = shellStream.Expect(new Regex(@"[$>]"));
+Console.WriteLine(output);
